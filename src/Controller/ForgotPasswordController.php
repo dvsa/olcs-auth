a3fc922327f75ec8840c57126d0dc57026ba7240
Richard Ward <richard.ward@bjss.com>
2017-10-09 14:01:37 +0100

    Reduce cyclomatic/Np complexity to satisfy review script
    For OLCS-17248
    
    Also improve type hinting in code to reduce IDE error highlights and
    aid navigability

--- src/Controller/ForgotPasswordController.php
+++ src/Controller/ForgotPasswordController.php
@@ -2,9 +2,11 @@
 
 namespace Dvsa\Olcs\Auth\Controller;
 
-use Common\Service\Cqrs\Exception;
-use Common\Service\Cqrs\Exception\NotFoundException;
+use Common\Service\Cqrs;
 use Dvsa\Olcs\Auth\Form\ForgotPasswordForm;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Zend\Form\Form;
+use Zend\Http\Request;
 use Zend\View\Model\ViewModel;
 use Dvsa\Olcs\Auth\Service\Auth\ForgotPasswordService;
 use Dvsa\Olcs\Transfer\Query\User\Pid;
@@ -13,57 +15,43 @@ use Dvsa\Olcs\Transfer\Query\User\Pid;
  * Forgot Password Controller
  *
  * @author Rob Caiger <rob@clocal.co.uk>
+ *
+ * @method Cqrs\Response handleQuery(QueryInterface $query)
  */
 class ForgotPasswordController extends AbstractController
 {
     /**
      * Forgot password page
      *
-     * @return ViewModel
+     * @return ViewModel|\Zend\Http\Response
      */
     public function indexAction()
     {
-        $request = $this->getRequest();
-
-        $form = $this->getServiceLocator()->get('Helper\Form')
-            ->createFormWithRequest(ForgotPasswordForm::class, $request);
-
-        if ($request->isPost() === false) {
-            return $this->renderView($form);
-        }
-
         if ($this->isButtonPressed('cancel')) {
             return $this->redirect()->toRoute('auth/login');
         }
 
+        /** @var Request $request */
+        $request = $this->getRequest();
+
+        /** @var Form $form */
+        $form = $this->getServiceLocator()->get('Helper\Form')
+            ->createFormWithRequest(ForgotPasswordForm::class, $request);
+
         $form->setData($request->getPost());
 
-        if ($form->isValid() === false) {
+        if ($request->isPost() === false || $form->isValid() === false) {
             return $this->renderView($form);
         }
 
-        $data = $form->getData();
+        $username = $form->getData()['username'];
 
-        try {
-            $response = $this->handleQuery(Pid::create(['id' => $data['username']]));
-        } catch (NotFoundException $e) {
-            // Mimic the OpenAM error message
-            return $this->renderView($form, true, 'User not found');
-        } catch (Exception $e) {
-            return $this->renderView($form, true, 'unknown-error');
+        $resetPreventionReason = $this->determineResetPreventionReason($username);
+        if (!is_null($resetPreventionReason)) {
+            return $this->renderView($form, true, $resetPreventionReason);
         }
 
-        if (!$response->isOk()) {
-            return $this->renderView($form, true, 'unknown-error');
-        }
-
-        $pidResult = $response->getResult();
-
-        if (!isset($pidResult['canResetPassword']) || $pidResult['canResetPassword'] !== true) {
-            return $this->renderView($form, true, 'account-not-active');
-        }
-
-        $result = $this->getForgotPasswordService()->forgotPassword($data['username']);
+        $result = $this->getForgotPasswordService()->forgotPassword($username);
         if ($result['status'] != 200) {
             return $this->renderView($form, true, $result['message']);
         }
@@ -82,13 +70,13 @@ class ForgotPasswordController extends AbstractController
     /**
      * Render the view
      *
-     * @param \Zend\Form\Form $form          Form
-     * @param bool            $failed        Failed
-     * @param string          $failureReason Failure reason
+     * @param Form   $form          Form
+     * @param bool   $failed        Failed
+     * @param string $failureReason Failure reason
      *
      * @return ViewModel
      */
-    private function renderView(\Zend\Form\Form $form, $failed = false, $failureReason = null)
+    private function renderView(Form $form, $failed = false, $failureReason = null)
     {
         $this->layout('auth/layout');
         $view = new ViewModel(['form' => $form, 'failed' => $failed, 'failureReason' => $failureReason]);
@@ -105,5 +93,37 @@ class ForgotPasswordController extends AbstractController
     private function getForgotPasswordService()
     {
         return $this->getServiceLocator()->get('Auth\ForgotPasswordService');
+    }
+
+    /**
+     * Determine any reason why the password reset may not continue
+     *
+     * @param string $username The username who's password is to be reset
+     *
+     * @return string|null Translatable reason why a user's password can't be reset, or null if it can be
+     */
+    private function determineResetPreventionReason($username)
+    {
+        try {
+            /** @var Cqrs\Response $response */
+            $response = $this->handleQuery(Pid::create(['id' => $username]));
+        } catch (Cqrs\Exception\NotFoundException $e) {
+            // Mimic the OpenAM error message
+            return 'User not found';
+        } catch (Cqrs\Exception $e) {
+            return 'unknown-error';
+        }
+
+        if (!$response->isOk()) {
+            return 'unknown-error';
+        }
+
+        $pidResult = $response->getResult();
+
+        if (!isset($pidResult['canResetPassword']) || $pidResult['canResetPassword'] !== true) {
+            return 'account-not-active';
+        }
+
+        return null;
     }
 }

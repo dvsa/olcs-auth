1cfa8e76a190e9709dd11e0303794b500d1e3ba6
Tonci Vidovic <tonci.vidovic@capgemini.com>
2019-02-11 17:29:29 +0000

    OLCS-16924 Welsh translations part 6

--- config/language/cy_GB.php
+++ config/language/cy_GB.php
@@ -1,6 +1,22 @@
 <?php
 
-# All these are in the common repo!
+# All these are in the common repo! <-(not sure they are..)
 
 return [
+    'auth.reset-password.failed.reason.Plug-in org.forgerock.openam.idrepo.ldap.DJLDAPv3Repo encountered an ldap exception 19: The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'0-9\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un rhif',
+    'auth.change-password.failed.reason.The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'0-9\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un rhif',
+    'auth.reset-password.failed.reason.Plug-in org.forgerock.openam.idrepo.ldap.DJLDAPv3Repo encountered an ldap exception 19: The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'A-Z\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un briflythyren',
+    'auth.change-password.failed.reason.The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'A-Z\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un briflythyren',
+    'auth.reset-password.failed.reason.Plug-in org.forgerock.openam.idrepo.ldap.DJLDAPv3Repo encountered an ldap exception 19: The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'a-z\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un llythyren fach',
+    'auth.change-password.failed.reason.The provided password value was rejected by a password validator:  The provided password did not contain enough characters from the character range \'a-z\'.  The minimum number of characters from that range that must be present in user passwords is 1'
+    => 'Rhaid i\'ch cyfrinair gynnwys o leiaf un llythyren fach',
+    'auth.change-password.failed.reason.Old password is incorrect.' => 'Mae\'r cyfrinair cyfredol yn anghywir',
+
+    //  Zend Error Messages
+    'The two given tokens do not match' => 'Nid yw\'r ddau gyfrinair yr un peth',
 ];
